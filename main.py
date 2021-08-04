b71ce39f2483894e90ae077efab91a59b7330f0c
Vasil Sotirov <vasil.d.sotirov@gmail.com>
2021-08-04 18:03:33 +0300

    Displaying blanks for each letter of chosen word and then replacing them if a letter is guessed.

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import random
 #Step 1
-def split(word):
-    return [char for char in word]
+#def split(word):
+#    return [char for char in word]
 word_list = ["aardvark", "baboon", "camel"]
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-
 chosen_word = word_list[random.randint(0,2)]
-chosen_word_list = split(chosen_word)
+print(f'Pssst, the solution is {chosen_word}.')
+#chosen_word_list = split(chosen_word)
 #print(chosen_word_list)
+
+display = []
+for blank in range (len(chosen_word)):
+    display.append("_")
+
+#print (display)
 choice_letter = input("Choose a letter:")
-for letter in chosen_word_list:
+test_num = 0
+for letter in chosen_word:
     if choice_letter == letter:
-        print ("Right")
-    else:
-        print ("Wrong")
+        display[test_num] = letter
+    test_num += 1
+print (display)
 
